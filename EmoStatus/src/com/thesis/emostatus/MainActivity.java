c7e80963aa1a1d69103e1bbfbadfe0ecd36427ba
Victor Rojas <vitorohe@gmail.com>
2013-10-07 16:40:09 -0300

    más pantallas

--- EmoStatus/src/com/thesis/emostatus/MainActivity.java
+++ EmoStatus/src/com/thesis/emostatus/MainActivity.java
@@ -1,15 +1,22 @@
 package com.thesis.emostatus;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Button login = (Button)findViewById(R.id.login);
+        login.setOnClickListener(this);
     }
 
 
@@ -19,5 +26,16 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+	public void onClick(View arg0) {
+
+		switch(arg0.getId()){
+			case R.id.login:
+			Intent login = new Intent(this,UserListActivity.class);
+//				start_game.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			this.startActivity(login);
+			finish();
+		}
+
+	}
     
 }
